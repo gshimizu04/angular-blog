766e793b00fcdfad5e128757722c0b7e9295aa31
Guilherme Comachio Shimizu <gshimizu04@gmail.com>
2023-09-20 22:28:13 -0300

    created content pages and their dynamic properties

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,7 +12,8 @@ export class SmallCardComponent implements OnInit {
   cardDate:string = ''
   @Input()
   cardTitle:string = ''
-
+  @Input()
+  Id:string = ''
   constructor() { }
 
   ngOnInit(): void {
